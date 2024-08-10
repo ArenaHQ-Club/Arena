6beeefc72648291f949ecdc6dfb1d25506031368
ShreyasPhadnis10 <phadnis.shreyas@gmail.com>
2024-08-10 14:14:26 +0530

    added redux to the website

--- codearena_website/src/App.js
+++ codearena_website/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./redux/Counter/CounterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>THe counter is: {count}</p>
+      <button
+        onClick={function incr() {
+          dispatch(increment());
+        }}
+      >
+        +
+      </button>
     </div>
   );
 }
