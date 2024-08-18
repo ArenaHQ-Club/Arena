0dd21f5a4720747767f5220e5f558379e2de2f92
ShreyasPhadnis10 <phadnis.shreyas@gmail.com>
2024-08-19 01:06:49 +0530

    Updated user Schema

--- Arena Backend/routes/Authentication.js
+++ Arena Backend/routes/Authentication.js
@@ -5,9 +5,8 @@ const bcrypt = require("bcrypt");
 
 router.post("/signup", async (req, res) => {
   try {
-    console.log("hi0");
     const { error } = validate(req.body);
-    console.log("hi1");
+
     if (error) {
       return res.status(400).send({ message: error.details[0].message });
     }
@@ -19,8 +18,9 @@ router.post("/signup", async (req, res) => {
 
     const salt = await bcrypt.genSalt(Number(process.env.SALT));
     const hashPassword = await bcrypt.hash(req.body.password, salt);
-
+    console.log("h2");
     await new User({ ...req.body, password: hashPassword }).save();
+    console.log("h1");
     res.status(200).send("User create succesfully");
   } catch (err) {
     res.status(500).send({ message: "Internal Server error" });
